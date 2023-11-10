78e334a87e7f0396bb4a18338de2eb857665d60a
nokcharathae <rkwmal0000@naver.com>
2023-11-11 05:41:39 +0900

    upload files

---
+++ Unreal_engine_cpp/WebSocketTest/MyGameInstance.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+#include "WebSocketsModule.h"
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	if (!FModuleManager::Get().IsModuleLoaded("WebSockets"))
+	{
+		FModuleManager::Get().LoadModule("WebSockets");
+	}
+	WebSocket = FWebSocketsModule::Get().CreateWebSocket("ws://localhost:8080");
+
+	WebSocket->OnConnected().AddLambda([]()
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Successfully connected");
+		});
+
+	WebSocket->OnConnectionError().AddLambda([](const FString& Error)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, Error);
+		});
+
+	WebSocket->OnClosed().AddLambda([](int32 StatusCode, const FString& Reason, bool bWasClean)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, bWasClean ? FColor::Green : FColor::Red, "Connection closed"+Reason);
+		});
+
+	WebSocket->OnMessage().AddLambda([](const FString& MessageString)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Cyan, "Received messsage"+MessageString);
+		});
+
+	WebSocket->OnMessageSent().AddLambda([](const FString& MessageString)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Sent messsage" + MessageString);
+		});
+
+	WebSocket->Connect();
+}
+
+void UMyGameInstance::Shutdown()
+{
+	if (WebSocket->IsConnected())
+	{
+		WebSocket->Close();
+	}
+	Super::Shutdown();
+}
